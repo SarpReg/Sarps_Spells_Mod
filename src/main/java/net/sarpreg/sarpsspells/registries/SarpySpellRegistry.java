494f6c7f5d22a9f6718bf2083a309bafe483138a
SarpReg <sarpguldag@gmail.com>
2026-05-19 17:55:50 +0300

    Added Spell: Updraft.

---
+++ src/main/java/net/sarpreg/sarpsspells/registries/SarpySpellRegistry.java
@@ -0,0 +1,26 @@
+package net.sarpreg.sarpsspells.registries;
+
+import io.redspace.ironsspellbooks.api.registry.SpellRegistry;
+import io.redspace.ironsspellbooks.api.spells.AbstractSpell;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.sarpreg.sarpsspells.SarpsSpellsMod;
+import net.sarpreg.sarpsspells.spells.UpdraftSpell;
+
+import java.util.function.Supplier;
+
+public class SarpySpellRegistry {
+    public static final DeferredRegister<AbstractSpell> SPELLS = DeferredRegister.create(SpellRegistry.SPELL_REGISTRY_KEY, SarpsSpellsMod.MODID);
+
+    public static Supplier<AbstractSpell> registerSpell(AbstractSpell spell) {
+        return SPELLS.register(spell.getSpellName(), () -> spell);
+    }
+
+    public static void register(IEventBus eventBus) {
+        SPELLS.register(eventBus);
+    }
+
+    // SPELL REGISTRY BELOW
+
+    public static final Supplier<AbstractSpell> UPDRAFT_SPELL = registerSpell(new UpdraftSpell());
+}
