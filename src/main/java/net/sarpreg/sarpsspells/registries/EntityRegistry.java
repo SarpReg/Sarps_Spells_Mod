8bd3e6ba8f5b7a7593a2da96d01670e5cc583d82
SarpReg <sarpguldag@gmail.com>
2026-06-09 21:28:45 +0300

    a

--- src/main/java/net/sarpreg/sarpsspells/registries/EntityRegistry.java
+++ src/main/java/net/sarpreg/sarpsspells/registries/EntityRegistry.java
@@ -1,8 +1,10 @@
 package net.sarpreg.sarpsspells.registries;
 
+import io.redspace.ironsspellbooks.IronsSpellbooks;
 import io.redspace.ironsspellbooks.entity.mobs.*;
 import io.redspace.ironsspellbooks.entity.spells.*;
 import io.redspace.ironsspellbooks.entity.spells.lightning_lance.LightningLanceProjectile;
+import io.redspace.ironsspellbooks.entity.spells.void_tentacle.VoidTentacle;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.EntityType;
@@ -11,6 +13,7 @@ import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.RegistryObject;
 import net.sarpreg.sarpsspells.SarpsSpellsMod;
+import net.sarpreg.sarpsspells.entity.spells.sculk_tendril.SculkTendril;
 import net.sarpreg.sarpsspells.entity.spells.sunlight_lance.SunlightLanceProjectile;
 
 
@@ -26,5 +29,11 @@ public class EntityRegistry {
                     .sized(1.25f, 1.25f)
                     .clientTrackingRange(64)
                     .build(ResourceLocation.fromNamespaceAndPath(SarpsSpellsMod.MODID, "sunlight_lance").toString()));
+
+    public static final RegistryObject<EntityType<SculkTendril>> SCULK_TENDRIL =
+            ENTITIES.register("sculk_tendril", () -> EntityType.Builder.<SculkTendril>of(SculkTendril::new, MobCategory.MISC)
+                    .sized(1f, 1f)
+                    .clientTrackingRange(64)
+                    .build(ResourceLocation.fromNamespaceAndPath(SarpsSpellsMod.MODID, "sculk_tendril").toString()));
 }
 
--- src/main/java/net/sarpreg/sarpsspells/registries/SarpySpellRegistry.java
+++ src/main/java/net/sarpreg/sarpsspells/registries/SarpySpellRegistry.java
@@ -24,7 +24,7 @@ public class SarpySpellRegistry {
 
     public static final Supplier<AbstractSpell> UPDRAFT_SPELL = registerSpell(new UpdraftSpell());
     public static final Supplier<AbstractSpell> LEVITATE_SPELL = registerSpell(new LevitateSpell());
-    //public static final Supplier<AbstractSpell> EMERGING_TENTACLES_SPELL = registerSpell(new EmergingTentaclesSpell());
+    public static final Supplier<AbstractSpell> EMERGING_TENTACLES_SPELL = registerSpell(new EmergingTentaclesSpell());
     public static final Supplier<AbstractSpell> SUNLIGHT_LANCE_SPELL = registerSpell(new SunlightLanceSpell());
     public static final Supplier<AbstractSpell> HOLY_DAGGER_RAIN_SPELL = registerSpell(new OldHolyDaggerRainSpell());
     public static final Supplier<AbstractSpell> LASER_EYES_SPELL = registerSpell(new LaserEyesSpell());
